6df25c5f7412445f22f900efe43936035265fb63
ryg <ryg.appdev@gmail.com>
2023-08-23 22:21:56 +0900

    Initial Commit

---
+++ HomeManager/HomeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeManagerApp.swift
+//  HomeManager
+//
+//  Created by 大橋諒雅 on R 5/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
